f44c2129f357221391628855abbec9d83b0c5469
Kelsinbek <janaevkelsinbek@gmail.com>
2024-12-24 14:15:06 +0600

    Initial commit

---
+++ src/main/java/kg/zarlykov/kelsinbek/payrollmanagementsystem/PayrollManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package kg.zarlykov.kelsinbek.payrollmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayrollManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PayrollManagementSystemApplication.class, args);
+    }
+
+}
